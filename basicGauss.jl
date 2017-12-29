dec1c85e3d6ca57c7a7ada26af602eff4f4709a7
krzywus <filip.krzywy@gmail.com>
2017-12-29 23:00:31 +0100

    basicGauss file cleanup

--- basicGauss.jl
+++ basicGauss.jl
@@ -1,16 +1,11 @@
-include("fileOperations.jl")
-using fileOperations
+module basicGauss
+
+export gaussElimination
 
 function gaussElimination(A, b, n, l)
-# A[wiersz, kolumna]
-#     — Gaussian Elimination —
     count = 0
     for k in 1:(n-1) # iterator kolumn
-        # println("gauss k: $k/$n")
         for i in (k+1):getLimit(k%l, k, l, n) # iterator wierszy, zaczynając od 'poniżej przekątnej' do /końca/wiadomych zer
-            # if A[i, k] == 0
-            #     println("CAUGHT A ZERO")
-            # end
             A[i,k] = A[i,k]/A[k,k]
             count += 1
             for j in (k+1):n
@@ -21,18 +16,16 @@ function gaussElimination(A, b, n, l)
             end
         end
     end
-    println("count:$count")
+    # println("count:$count")
     count += forward(A, b, n, l)
-    println("count:$count")
+    # println("count:$count")
     count += backward(A, b, n, l)
-    println("count:$count")
+    println("Operations count: $count")
 end
 
 function forward(A, b, n, l)
-# — Forward Elimination —
     count = 0
     for i in 1:(n-1)
-        # println("forward i: $i/$n")
         for j in (i+1):getLimit(i%l, i, l, n)
             b[j] -= A[j,i] * b[i]   # b[i] = x[i], mnożenie pozostałych przez ten współczynnik
             count += 1
@@ -42,10 +35,8 @@ function forward(A, b, n, l)
 end
 
 function backward(A, b, n, l)
-#     — Backward Solve —
     count = 0
     for i in n:-1:1
-        # println("backward u: $(n-i)/$n")
         for j in (i+1):min(i+l, n)
             b[i] -= A[i,j]*b[j]
             count += 1
@@ -66,25 +57,4 @@ function getLimit(mod, i, l, n)
     end
 end
 
-# A = [2.0    -2.0   0.0;
-#      -2    0    2;
-#      0   -2   0]
-# N=3
-# l=1
-# b = [6, 0, -2]
-# sparseA = sparse(copy(A))
-if size(ARGS)[1] > 0
-    sizeArg = parse(Int, ARGS[1])
-    sparseA, N, l = readMatrixFromFile("data/$(sizeArg)_A.txt")
-    b = readVectorFromFile("data/$(sizeArg)_B.txt")
-else
-    sparseA, N, l = readMatrixFromFile("data/1000_A.txt")
-    b = readVectorFromFile("data/1000_B.txt")
 end
-# writeMatrixToFile(sparseA, "starting.csv")
-tic()
-gaussElimination(sparseA, b, N, l)
-toc()
-# writeMatrixToFile(sparseA, "comp.csv")
-println(b[1:11])
-println(b[size(b)[1]:-1:size(b)[1]-10])
