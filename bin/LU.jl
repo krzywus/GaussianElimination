58c97d831453948c76803455b261179b0763386f
krzywus <filip.krzywy@gmail.com>
2018-01-02 21:39:23 +0100

    fixed LU decomposition to some great performance

--- bin/LU.jl
+++ bin/LU.jl
@@ -1,128 +1,106 @@
 # module gauss
+include("fileOperations.jl")
+using fileOperations
 
-# export readMatrixFromFile, writeMatrixToFile
-
-# funkcja do zczytywania macierzy A
-function readMatrixFromFile(filename::String)
-    println("Reading file $filename.")
-    open(filename) do f
-        lines = readlines(f)
-        firstLine = lines[1]
-        deleteat!(lines, 1)
-        I = zeros(0)
-        J = zeros(0)
-        V = zeros(0)
-#         k = 1 # debug
-        n = parse(Int64, split(firstLine, r"\ |\t")[1])
-        l = parse(Int64, split(firstLine, r"\ |\t")[2])  # debug
-#         Q = zeros(Float64, n, n) # debug
-        for line in lines
-            splitLine = split(line, r"\ |\t")
-#             println(splitLine[1], "|", splitLine[2], "|", splitLine[3])
-            push!(I, parse(Int64, splitLine[1]))
-            push!(J, parse(Int64, splitLine[2]))
-            push!(V, parse(Float64, splitLine[3]))
-#             println(I[k], "|", J[k], "|", V[k])
-#             Q[Int(I[k]), Int(J[k])] = V[k]
-#             k += 1
-           end
-        println("Done reading file $filename.")
-        return sparse(I,J,V), n, l
-    end
-end
+# export LUdecompose
 
 
-function readVectorFromFile(filename::String)
-    println("Reading file $filename.")
-    open(filename) do f
-        lines = readlines(f)
-        firstLine = lines[1]
-        deleteat!(lines, 1)
-        V = zeros(0)
-#         n = parse(Int64, split(firstLine, r"\ |\t")[1])
-        for line in lines
-            push!(V, parse(Float64, line))
-           end
-        println("Done reading file $filename.")
-        return V
-    end
-end
-
-function writeMatrixToFile(matrix, filename, delim="\t")
-    println("Writing file $filename.")
-    open(filename, "w") do file
-          writedlm(file, matrix, delim)
-    end
-    println("Done writing file $filename.")
-end
-
-
-function LUdecompose(A, n)
+function LUdecompose(A, n, l)
     # A[wiersz, kolumna]
-    for i in 1:(n-1)  # iterator kolumn
-        println("LUdecompose i: $i")
-        for j in (i+1):n # utworzenie współczyników L i przechowanie poniżej przekątnej
-            if A[j, i] == 0
-                continue
-            end
+    count = 0
+    for i in 1:(n-1)
+        for j in (i+1):getLimit(i%l, i, l, n) # utworzenie współczyników L i przechowanie poniżej przekątnej
             A[j,i] = A[j,i]/A[i,i]
+            count += 1
         end
-        for k in (i+1):n         # odjęcie od równań równań pomnożonych przez współczynnik
-            for j in (i+1):n
+        for k in (i+1):min(n, i+l)         # odjęcie od równań równań pomnożonych przez współczynnik
+            for j in (i+1):min(i+l+1, n)
                 A[j,k] = A[j,k] - A[j,i] * A[i,k]
+                count += 2
             end
         end
     end
+    println("count: $count")
 end
 
 
 # calculates Y and stores it in vector B. diagonal element of L are always ones.
 function solveLyb(A, b, n)
+    count = 0
     for i in 2:n
-        println("solveLyb i: $i")
         Left = 0
-        for j in i-1:-1:1
-            Left += i==j ? b[j] : A[i, j] * b[j]
+        for j in i-1:-1:max(1, i-l-1)
+            if A[i, j] == 0
+                continue
+            end
+            Left += A[i, j] * b[j]
+            count += 2
         end
         b[i] = (b[i]-Left)   # /A[i,i],  /1.0
     end
+    println("count: $count")
 end
 
 # calculates X and stores it in vector Y
 function solveUxy(A, y, n)
+    count = 0
     y[n] = y[n]/A[n,n]
     for i in (n-1):-1:1
-        println("solveUxy i: $i")
         Left = 0
-        for j in (i+1):n
+        for j in (i+1):min(i+l, n)
             Left += A[i, j] * y[j]
+            count += 2
         end
         y[i] = (y[i]-Left)/A[i,i]
     end
+    println("count: $count")
 end
-# end
 
-#             if floor(j-1/n) == floor(i/j) # jesteśmy w A
-#                 A[j,i] = A[j,i]/A[i,i]
-#                continue
-#             end
+function getLimit(mod, i, l, n)
+    if mod == l-1
+        return min(i+1+l, n)
+    elseif mod == 0
+        return min(i+l, n)
+    else
+        return min(i-mod+l, n)
+    end
+end
 
+function getNonZeroElementsIndexes(i, n, l)
+    imodl = i%l
+    if imodl == 0
+        limit = collect(max(1, i-l-1):i)
+    else
+        limit = collect(max(1, i-imodl-1):min(n, i+l-imodl))
+    end
+    if n >= i+l
+        push!(limit, i+l)
+    end
+    return limit
+end
 
 # sparseA, N, l = readMatrixFromFile("data/100_A.txt")
 # b = readVectorFromFile("data/100_B.txt")
-sparseA, N, l = readMatrixFromFile("data/medA.txt")
-b = readVectorFromFile("data/medB.txt")
+if size(ARGS)[1] < 2
+    println("Not enough arguments")
+    exit(1)
+end
+sparseA, N, l = readMatrixFromFile(ARGS[1])
+b = readVectorFromFile(ARGS[2])
 
+println("LUdecompose")
 tic()
-# writeMatrixToFile(sparseA, "starting.csv")
-LUdecompose(sparseA, N)
-# writeMatrixToFile(sparseA, "comp.csv")
-solveLyb(sparseA, b, N)
-# writeMatrixToFile(b, "calc1.csv")
-# println("y: $b")
-solveUxy(sparseA, b, N)
-# writeMatrixToFile(b, "calc2.csv")
+LUdecompose(sparseA, N, l)
 toc()
-println("x: $b")
+println("solveLyb")
+tic()
+solveLyb(sparseA, b, N)
+toc()
+println("solveUxy")
+tic()
+solveUxy(sparseA, b, N)
+toc()
+println(b[1:11])
+println(b[size(b)[1]:-1:size(b)[1]-10])
 
 # println(sparseA)
